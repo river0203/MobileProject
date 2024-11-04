c92fc9e81eb05fffe0ee28e9a87b3c35612fc186
Goospel <kimsadol369785@gmail.com>
2024-11-04 11:41:02 +0900

    create big 4 pages
    1. 맨 처음 선택 페이지
    2. 냉장고에 재료 추가 페이지
    3. GPT 레시피 제공 페이지
    4. 마지막 냉장고 정리 페이지

--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.selecte_activity_page);
     }
 }
